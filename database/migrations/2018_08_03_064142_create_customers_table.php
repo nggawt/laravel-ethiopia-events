d40913ac1f8a00f348ddb6007ad065f5e91ddd8c
nggawt <nggawt001@gmail.com>
2018-12-20 05:36:44 +0200

    fix db seeder and migrations

---
+++ database/migrations/2018_08_03_064142_create_customers_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('company');
+            $table->string('businessType');
+            $table->string('loggo');
+            $table->string('title');
+            $table->string('contact');
+            $table->text('discription');
+            $table->string('email')->unique();
+            $table->string('tel');
+            $table->string('address');
+            $table->text('deals');
+
+            $table->foreign('user_id')
+              ->references('id')->on('users')
+              ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+            // $table->integer('user_id')->default(1);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
